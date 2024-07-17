528a4f6114aaf7ff62b127769d59af941553d379
ibrahim-mohamed-rezk <ibrahimmo989800@gmail.com>
2024-07-17 21:50:55 +0300

    profile section

--- resources/views/web/pages/profile/download.blade.php
+++ resources/views/web/pages/profile/download.blade.php
@@ -1,5 +1,20 @@
 @extends('web.pages.account')
 
 @section('profileContent')
-    <h2>hello download</h2>
+<div class="account-dwonload-container">
+    @for ($i = 0; $i < 10; $i++) <div class="product-container">
+        <div class="product-overlay">
+            <button class="view">VIEW</button>
+            <button class="add-to-cart">ADD TO CART</button>
+        </div>
+        <img class="product-image" src="{{asset('storage/assets/download.png')}}" alt="Product Image">
+        <div class="product-info">
+            <div class="product-title">Modern Wooden Storage Bench | 2 Drawers</div>
+            <div class="product-category">Shoes Cabinet</div>
+        </div>
+</div>
+@endfor
+
+</div>
+
 @endsection
